150a343ddb7fe7f751b82e4a4043ae2249caceb0
HosteMatthieu <matthieu.hoste@student.junia.com>
2023-03-28 23:42:25 +0200

    fin etape 2

--- calculatrice.js
+++ calculatrice.js
@@ -1,38 +1,43 @@
-//fonction qui va ecrire les caractères sur lesquelles on clique dans le input
-//prend en paramètre une value qui correspond au caractère sur lequel on clique
-function addToScreen(value){
-    //on ajoute au screen l'argument de la fonction
-    document.getElementById('screen').value+=value;
-    document.getElementById('result').value="";
-}
-
-//fonction qui vide le screen
-function clearAllScreen(){
-    document.getElementById('screen').value="";
-}
-
-//fonction qui va uniquement supprimer le dernier élément 
-function clearLastNumber(){
-    //On récupère l'expression
-    var screen=document.getElementById('screen').value;
-    //On récupère la longueur de l'expression
-    var lengthScreen=screen.length;
-    //Et on utilise substring avec lengthScreen-1 pour supprimer uniquement le dernier élément
-    document.getElementById('screen').value = screen.substring(0,lengthScreen-1);
-}
-
-//fonction qui calcul et affiche le résultat
-function resultScreen(){
-    //calcul grâce à la fonction eval
-    var resultat=eval(document.getElementById('screen').value);
-    //gestion erreur quand resultat est pas définit
-    if(resultat==undefined){
-        document.getElementById('result').value="ERROR";
+class BaseCalculator{
+    constructor(){
+        this.screen=document.getElementById('screen')
+        this.result=document.getElementById('result')
+        this.history=[]
     }
-    //Si pas d'erreurs on affiche le résultat de le input result
-    else{
-        document.getElementById('result').value=resultat;
+
+    //fonction qui va ecrire les caractères sur lesquelles on clique dans le input
+    //prend en paramètre une value qui correspond au caractère sur lequel on clique
+    addToScreen(value){
+        this.screen.value+=value;
+        this.result.value="";
     }
-    //On vide screen
-    document.getElementById('screen').value="";
+
+    //fonction qui calcul et affiche le résultat
+    resultScreen(){
+        var resultat=eval(this.screen.value);
+        this.history.push(this.screen.value)
+        this.result.value=resultat;
+        this.screen.value="";
+    }
+    
+    //fonction qui vide le screen
+    clearAllScreen(){
+        this.screen.value="";
+    }
+
+    //fonction qui va uniquement supprimer le dernier élément 
+    clearLastNumber(){
+        var valueScreen=this.screen.value;
+        var lengthScreen=valueScreen.length;
+        this.screen.value = valueScreen.substring(0,lengthScreen-1);
+    }
+
+    backHistory(){
+        let elem=this.history.pop()
+        if(elem!=undefined){
+        this.screen.value=elem;            
+        }
+    }
 }
+
+let calculator=new BaseCalculator
